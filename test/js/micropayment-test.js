6bf6a73bbf8a61945ec83cad708f40d7ba1cc6fd
docbull <fudghks21@gmail.com>
2022-09-03 23:32:54 +0900

    message sign using metamask

--- test/js/micropayment-test.js
+++ test/js/micropayment-test.js
@@ -119,47 +119,47 @@ function signMessage(message, callback) {
     )
 }
 
-// contractAddress is used to prevent cross-contract replay attacks.
-// amount, in wei, specifies how much Ether should be sent.
-function signPayment(contractAddress, amount, callback) {
-    var message = constructPaymentMessage(contractAddress, amount)
-    signMessage(message, callback)
-}
+// // contractAddress is used to prevent cross-contract replay attacks.
+// // amount, in wei, specifies how much Ether should be sent.
+// function signPayment(recipient, contractAddress, amount, nonce, callback) {
+//     var message = constructPaymentMessage(contractAddress, amount)
+//     signMessage(message, callback)
+// }
 
-// this mimics the prefixing behavior of the eth_sign JSON-RPC method.
-function prefixed(hash) {
-    return abi.soliditySHA3(
-        ["string", "bytes32"],
-        ["\x19Ethereum Signed Message:\n32", hash]
-    )
-}
+// // this mimics the prefixing behavior of the eth_sign JSON-RPC method.
+// function prefixed(hash) {
+//     return abi.soliditySHA3(
+//         ["string", "bytes32"],
+//         ["\x19Ethereum Signed Message:\n32", hash]
+//     )
+// }
 
-function recoverSigner(message, signature) {
-    var split = ethereumjs.Util.fromRpcSig(signature);
-    var publicKey = ethereumjs.Util.ecrecover(message, split.v, split.r, split.s);
-    var signer = ethereumjs.Util.pubToAddress(publicKey).toString("hex");
-    return signer;
-}
+// function recoverSigner(message, signature) {
+//     var split = ethereumjs.Util.fromRpcSig(signature);
+//     var publicKey = ethereumjs.Util.ecrecover(message, split.v, split.r, split.s);
+//     var signer = ethereumjs.Util.pubToAddress(publicKey).toString("hex");
+//     return signer;
+// }
 
-function isValidSignature(contractAddress, amount, signature, expectedSigner) {
-    var message = prefixed(constructPaymentMessage(contractAddress, amount));
-    var signer = recoverSigner(message, signature);
-    return signer.toLowerCase() ==
-        ethereumjs.Util.stripHexPrefix(expectedSigner).toLowerCase();
-}
-
-export { signPayment }
+// function isValidSignature(contractAddress, amount, signature, expectedSigner) {
+//     var message = prefixed(constructPaymentMessage(contractAddress, amount));
+//     var signer = recoverSigner(message, signature);
+//     return signer.toLowerCase() ==
+//         ethereumjs.Util.stripHexPrefix(expectedSigner).toLowerCase();
+// }
 
 // recipient is the address that should be paid.
 // amount, in wei, specifies how much ether should be sent.
 // nonce can be any unique number to prevent replay attacks
 // contractAddress is used to prevent cross-contract replay attacks
-// function signPayment(recipient, amount, nonce, contractAddress, callback) {
-//     // var hash = web3.utils.sha3("message to sign")
-//     var hash = "0x" + abi.soliditySHA3(
-//         ["address", "uint256", "uint256", "address"],
-//         [recipient, amount, nonce, contractAddress]
-//     ).toString("hex")
+function signPayment(recipient, amount, nonce, contractAddress, callback) {
+    // var hash = web3.utils.sha3("message to sign")
+    var hash = "0x" + abi.soliditySHA3(
+        ["address", "uint256", "uint256", "address"],
+        [recipient, amount, nonce, contractAddress]
+    ).toString("hex")
+    
+    web3.eth.personal.sign(hash, window.ethereum.selectedAddress, '');
+}
 
-//     web3.eth.personal.sign(hash, web3.eth.defaultAccount, callback);
-// }
+export { signPayment }
